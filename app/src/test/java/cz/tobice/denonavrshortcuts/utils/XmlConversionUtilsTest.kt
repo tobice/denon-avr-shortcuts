be954aac219f053d74587dd0b643e176fcd09e64
Tobiáš Potoček <tobiaspotocek@gmail.com>
2022-05-24 21:18:14 +0200

    test: add tests for XML conversion utils

--- app/src/test/java/cz/tobice/denonavrshortcuts/utils/XmlConversionUtilsTest.kt
+++ app/src/test/java/cz/tobice/denonavrshortcuts/utils/XmlConversionUtilsTest.kt
@@ -5,11 +5,35 @@ import io.kotest.matchers.shouldBe
 import org.junit.Test
 
 class XmlConversionUtilsTest {
-    // TODO: Implement proper tests
 
     @Test
     fun `toXML returns an unformatted XML string`() {
         toXml(SurroundParameterConfig(centerSpread = "1")) shouldBe
             "<SurroundParameter><CenterSpread>1</CenterSpread></SurroundParameter>"
+    }
+
+    @Test
+    fun `oneZeroToBoolean converts 1 to true`() {
+        oneZeroToBoolean("1") shouldBe true
+    }
+
+    @Test
+    fun `oneZeroToBoolean converts 0 to false`() {
+        oneZeroToBoolean("0") shouldBe false
+    }
+
+    @Test
+    fun `oneZeroToBoolean converts unsupported value to nul`() {
+        oneZeroToBoolean("unsupported value") shouldBe null
+    }
+
+    @Test
+    fun `booleanToOneZero converts true to 1`() {
+        booleanToOneZero(true) shouldBe "1"
+    }
+
+    @Test
+    fun `booleanToOneZero converts false to 0`() {
+        booleanToOneZero(false) shouldBe "0"
     }
 }
